2fd2f55e5d62e07c0a985aa8a564cef00edeacda
Giusephe <giusephesilva@hotmail.com>
2026-09-07 14:54:46 -0300

    PHP COMPOSER FERRAMENTAS/SCRIPTS

--- src/Buscador.php
+++ src/Buscador.php
@@ -5,26 +5,28 @@ namespace Alura\BuscadorDeCursos;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador{
+class Buscador
+{
     private $httpClient;
     private $crawler;
 
-    public function __construct(ClientInterface $httpClient, Crawler $crawler){
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
         $this->httpClient = $httpClient;
         $this->crawler = $crawler;
     }
-  
-    public function buscar(string $url):array{
+    public function buscar(string $url): array
+    {
         $resposta = $this->httpClient->request('GET', $url);
         $html =  $resposta->getBody();
         $this->crawler->addHtmlContent($html);
 
-        $elementosCursos = $this->crawler->filter('h3.line-clamp-2');  
-        
+        $elementosCursos = $this->crawler->filter('h3.line-clamp-2');
+
         $cursos = [];
 
-        foreach($elementosCursos as $elemento){
-            $cursos[] = $elemento->textContent.PHP_EOL;
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent . PHP_EOL;
         }
         return $cursos;
     }
--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,9 +6,20 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
+    '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
+    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
+    '8825ede83f2f289127722d4e842cf7e8' => $vendorDir . '/symfony/polyfill-intl-grapheme/bootstrap.php',
+    'e69f7f6ee287b969198c3c9d6777bd38' => $vendorDir . '/symfony/polyfill-intl-normalizer/bootstrap.php',
     'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
     '5897ea0ac4cccf14d323035e65887801' => $vendorDir . '/symfony/polyfill-php82/bootstrap.php',
-    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
-    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
-    '644dba18169707cb176cc8c4a61a581f' => $baseDir . '/functions.php',
+    'b6b991a57620e2fb6b2f66f03fe9ddc2' => $vendorDir . '/symfony/string/Resources/functions.php',
+    '606a39d89246991a373564698c2d8383' => $vendorDir . '/symfony/polyfill-php85/bootstrap.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+    'c96deaca55ead23750db7143dd4212b8' => $vendorDir . '/phan/var_representation_polyfill/src/var_representation.php',
+    '2b9d0f43f9552984cfa82fee95491826' => $vendorDir . '/sabre/event/lib/coroutine.php',
+    'd81bab31d3feb45bfe2f283ea3c8fdf7' => $vendorDir . '/sabre/event/lib/Loop/functions.php',
+    'a1cce3d26cc15c00fcd0b3354bd72c88' => $vendorDir . '/sabre/event/lib/Promise/functions.php',
+    '14256cea64544233ca49f37177c0ad3b' => $vendorDir . '/phan/phan/src/Phan/bootstrap/polyfills.php',
+    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
 );
